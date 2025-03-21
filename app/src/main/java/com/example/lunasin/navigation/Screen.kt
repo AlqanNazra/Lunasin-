aeed218bfe100d3a997ac82a38744e7075887d07
Alqan Nazra <alqan.nazra02@gmail.com>
2025-03-21 11:55:53 +0700

    Menambahkan branc baru dan membuat struktur file baru

--- app/src/main/java/com/example/lunasin/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lunasin.navigation
-
-sealed class Screen(val route: String) {
-    object Login : Screen("login_screen")
-    object Home : Screen("home_screen")
-    object SignUp : Screen("signup_screen")
-}
